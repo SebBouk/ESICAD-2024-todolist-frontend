62995fa36e271239ba7d888ac569b30591cf8681
Aurélien VERNAY <18377871+AurelienVernay@users.noreply.github.com>
2024-09-09 15:16:38 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import TodoComponent from './components/TodoComponent.vue';
+const monTableau = ref([{ todo: 'apprendre Vue Js', done: false },
+{ todo: 'apprendre à faire des boucles', done: false },
+{ todo: 'apprendre à griller des saucisses', done: true }]);
+
+// const monTableau = [1, 2, 3];
+
+const onTodoInput = (newTodoValue: any, index: number) => {
+  monTableau.value[index] = newTodoValue;
+  console.log('monTableau est mis à jour');
+}
+
+</script>
+
+<template>
+  <p>Hello World !</p>
+  <span v-if="monTableau.length % 2 === 0">Mon tableau est pair</span>
+  <span v-else>Mon tableau est impair</span>
+  <br />
+  <TodoComponent v-for="(element, index) in monTableau" :todo="element" v-bind:key="index"
+    @onInput="onTodoInput($event, index)" />
+
+  <!-- {{ monTableau }} -->
+
+</template>
+
+<style scoped></style>
---
+++ src/components/TodoComponent.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+    todo: {
+        done: boolean,
+        todo: string
+    }
+}>();
+
+const emit = defineEmits(['onInput'])
+const onInput = (value: boolean) => {
+    console.log('TodoComponent a détecté un changement ', value);
+    emit('onInput', value)
+}
+</script>
+
+<template>
+    <span>
+        {{ props.todo.todo }}
+        <input type="checkbox" :checked="props.todo.done" @click="(event: any) => onInput(event.target?.checked)" />
+        <br />
+    </span>
+</template>
+
+<style lang="css" scoped>
+.done {
+    color: green;
+}
+</style>
